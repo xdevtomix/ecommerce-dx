356b19d9a31eff195ccff16353215b9558280969
tomi <dev.dreissiger@gmail.com>
2021-10-12 15:28:40 +0200

    main, home in

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+        <section className="home" id="home">
+            <div className="home__container bd-grid">
+                <div className="home__data">
+                    <h1 className="home__title"> NEW <br /> <span>ARRIVALS</span></h1>
+                    <a href="#featured" className="button">GO SHOPPING</a>
+                </div>
+
+                <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="woman" className="home__img" />
+            </div>
+        </section>
+    );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Home from "./Home";
+import Collection from "./Collection";
+
+export default function Main() {
+    return (
+        <main data-component="main" className="l-main">
+            <Home />
+            <Collection />
+        </main>
+    );
+}
