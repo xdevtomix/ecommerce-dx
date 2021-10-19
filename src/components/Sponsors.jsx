76741c09a54d2e11122c9a8113d4fd77352bd9c5
tomi <dev.dreissiger@gmail.com>
2021-10-19 14:23:52 +0200

    sponsors ready

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -1,20 +1,61 @@
+import styled from "styled-components";
+
 export default function Sponsors() {
     return (
-        <section className="sponsors section">
-            <div className="sponsors__container bd-grid">
-                <div className="sponsors__logo">
+        <Container data-component="sponsors">
+            <GridContainer>
+                <Logo>
                     <i className='bx bxl-google'></i>
-                </div>
-                <div className="sponsors__logo">
+                </Logo>
+                <Logo>
                     <i className='bx bxl-twitter'></i>
-                </div>
-                <div className="sponsors__logo">
+                </Logo>
+                <Logo>
                     <i className='bx bxl-windows'></i>
-                </div>
-                <div className="sponsors__logo">
+                </Logo>
+                <Logo>
                     <i className='bx bxl-bing'></i>
-                </div>
-            </div>
-        </section>
+                </Logo>
+            </GridContainer>
+        </Container>
     );
 }
+
+const Container = styled.section`
+    padding: 3rem 0;
+`;
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    row-gap: 2rem;
+    column-gap: 2rem;
+    width: calc(100% - 3rem);
+    max-width: 1024px;
+    padding: 1.5rem 0;
+    margin-left: var(--mb-3);
+    margin-right: var(--mb-3);    
+    
+    @media screen and (min-width: 768px) {
+        grid-template-columns: repeat(4, 1fr);
+    }
+
+    @media screen and (min-width: 1024px) {
+        margin-left: auto;
+        margin-right: auto;
+    }
+`;
+
+const Logo = styled.div`
+    display: flex;
+    justify-content: center;
+
+    i {
+        font-size: 80px;
+        filter: opacity(70%);
+
+        :hover {
+            filter: none;
+        }
+    }
+`;
