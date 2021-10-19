27e371e07b30dfaf79ecb41b657d72cafe1a4769
tomi <dev.dreissiger@gmail.com>
2021-10-19 15:36:19 +0200

    new ready

--- src/components/New.jsx
+++ src/components/New.jsx
@@ -1,52 +1,140 @@
+import styled from "styled-components";
+
 export default function New() {
     return (
-        <section className="new section" id="new">
-            <h2 className="section-title">NEW ARRIVALS</h2>
-            <a href="#" className="section-all">View All</a>
+        <Container data-component="new" id="new">
+            <Title>NEW ARRIVALS</Title>
+            <SubTitle href="#">View All</SubTitle>
 
-            <div className="new__container bd-grid">
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/8091191/pexels-photo-8091191.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="laptop" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
+            <GridContainer>
+                <Box>
+                    <img src="https://images.pexels.com/photos/8091191/pexels-photo-8091191.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="laptop" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
 
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/8473285/pexels-photo-8473285.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="phone" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
+                <Box>
+                    <img src="https://images.pexels.com/photos/8473285/pexels-photo-8473285.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="phone" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
 
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/5552789/pexels-photo-5552789.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="computer" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
+                <Box>
+                    <img src="https://images.pexels.com/photos/5552789/pexels-photo-5552789.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="computer" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
 
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/4678187/pexels-photo-4678187.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="camera" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
+                <Box>
+                    <img src="https://images.pexels.com/photos/4678187/pexels-photo-4678187.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="camera" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
 
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/9704415/pexels-photo-9704415.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="playstation" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
+                <Box>
+                    <img src="https://images.pexels.com/photos/9704415/pexels-photo-9704415.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="playstation" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
 
-                <div className="new__box">
-                    <img src="https://images.pexels.com/photos/9287210/pexels-photo-9287210.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="show and phone" className="new__img" />
-                    <div className="new__link">
-                        <a href="#" className="button">VIEW PRODUCT</a>
-                    </div>
-                </div>
-            </div>
-        </section>
+                <Box>
+                    <img src="https://images.pexels.com/photos/9287210/pexels-photo-9287210.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="show and phone" />
+                    <Link>
+                        <Button href="#">VIEW PRODUCT</Button>
+                    </Link>
+                </Box>
+            </GridContainer>
+        </Container>
     );
 }
+
+const Container = styled.section`
+    padding: 3rem 0;
+`;
+
+const Title = styled.h2`
+    font-size: var(--h2-font-size);
+    color: var(--dark-color);
+    margin: var(--mb-4) 0 var(--mb-1);
+    text-align: center;
+`;
+
+const SubTitle = styled.a`
+    display: block;
+    font-size: var(--small-font-size);
+    font-weight: var(--font-semi);
+    color: var(--dark-color-light);
+    text-align: center;
+    margin-bottom: var(--mb-2);
+
+    @media screen and (min-width: 768px) {
+        margin-bottom: var(--mb-4);
+    }
+`;
+
+const GridContainer = styled.div`
+    display: grid;
+    grid-template-columns: 100%;
+    column-gap: 2rem;
+    row-gap: 2rem;
+    width: calc(100% - 3rem);
+    max-width: 1024px;
+    margin-left: var(--mb-3);
+    margin-right: var(--mb-3);
+
+    @media screen and (min-width: 768px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media screen and (min-width: 1024px) {
+        margin-left: auto;
+        margin-right: auto;
+    }
+`;
+
+const Box = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    background-color: var(--gray-color);
+    overflow: hidden;
+
+    :hover {
+        bottom: 0;
+    }
+`;
+
+const Link = styled.div`
+    position: absolute;
+    bottom: -100%;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(229, 89, 71, 0.2);
+    transition: 0.3s;
+
+    :hover {
+        bottom: 0;
+    }
+`;
+
+const Button = styled.a`
+    display: inline-block;
+    background-color: var(--first-color);
+    color: var(--white-color);
+    padding: 1rem;
+    font-size: var(--small-font-size);
+    font-weight: var(--font-semi);
+    transition: 0.3s;
+
+    :hover {
+        background-color: var(--first-color-alt);
+    }
+`;
