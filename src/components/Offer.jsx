3509f35d7e4c98f261632962f51ffc93f22dc715
tomi <dev.dreissiger@gmail.com>
2021-10-19 14:40:24 +0200

    offer ready

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,14 +1,61 @@
+import styled from "styled-components";
+
 export default function Offer() {
     return (
-        <section className="offer section">
-            <div className="offer__bg">
-                <div className="offer__data">
-                    <h2 className="offer__title">Special Offer</h2>
-                    <p className="offer__description">Special offer discounts for women this week only</p>
+        <Container data-component="offer">
+            <Background>
+                <Data>
+                    <Title>Special Offer</Title>
+                    <Description>Special offer discounts for women this week only</Description>
 
-                    <a href="#" className="button">SHOP NOW</a>
-                </div>
-            </div>
-        </section>
+                    <Button href="#">SHOP NOW</Button>
+                </Data>
+            </Background>
+        </Container>
     );
 }
+
+const Container = styled.section`
+    padding: 3rem 0;
+`;
+
+const Background = styled.div`
+    background: url('https://images.pexels.com/photos/935756/pexels-photo-935756.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940') no-repeat center/cover;
+    text-align: center;
+`;
+
+const Data = styled.div`
+    padding: 3.5rem 0.25rem;
+    background: rgba(0, 0, 0, 0.5);
+    color: var(--white-color);
+
+    @media screen and (min-width: 768px) {
+        padding: 5rem 0;
+    }
+`;
+
+const Title = styled.h2`
+    font-size: var(--h1-font-size);
+    letter-spacing: 0.2rem;
+    text-transform: uppercase;
+    font-weight: var(--font-semi);
+`;
+
+const Description = styled.p`
+    margin-bottom: var(--mb-2);
+    letter-spacing: 0.2rem;
+`;
+
+const Button = styled.a`
+    display: inline-block;
+    background-color: var(--first-color);
+    color: var(--white-color);
+    padding: 1rem;
+    font-size: var(--small-font-size);
+    font-weight: var(--font-semi);
+    transition: 0.3s;
+
+    :hover {
+        background-color: var(--first-color-alt);
+    }
+`;
